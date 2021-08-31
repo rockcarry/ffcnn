4bfc24b969e3df081a9d0ad8e1169dc3517aef66
ck <rockcarry@163.com>
2021-08-31 11:32:40 +0800

    优化 im2row 代码，优化卷积运算代码，速度提升 10ms 左右

--- ffcnn/ffcnn.c
+++ ffcnn/ffcnn.c
@@ -8,10 +8,12 @@
 #ifdef _MSC_VER
 #pragma warning(disable:4996)
 #define snprintf _snprintf
-#endif
 typedef int                int32_t;
 typedef unsigned           uint32_t;
 typedef unsigned long long uint64_t;
+#else
+#include <stdint.h>
+#endif
 
 #ifdef WIN32
 #include <windows.h>
@@ -207,7 +209,7 @@ NET* net_load(char *fcfg, char *fweights)
             }
             ilayer->type = LAYER_TYPE_YOLO;
         } else got_layer = 0;
-		if (got_layer) layercur++;
+        if (got_layer) layercur++;
         pstart = pend;
     }
     free(cfgstr);
@@ -342,30 +344,18 @@ static float activate(float x, int type)
     }
 }
 
-static void im2row(float *data, int w, int h, int c, int pad, int fs, int x, int y, float *buf)
+static void im2row(float *img, int w, int h, int c, int pad, int fs, int stride, int y, float *buf)
 {
-    int i, j, k;
-    data += (y - pad) * w + (x - pad);
-    if (pad == 0 || (x - pad >= 0 && y - pad >= 0 && x + pad < w && y + pad < h)) {
-        i = fs * fs * c;
-        j = k = 0;
-        do {
-            *buf++ = *data++;
-            if (++k == fs) {
-                if (1)         { k = 0; data += w - fs; }
-                if (++j == fs) { j = 0; data += w * (h - fs); }
+    int  i, j, k, x;
+    for (i=0; i<c; i++) {
+        for (j=0; j<fs; j++) {
+            float *src = img + i * w * h + (y - pad + j) * w - pad;
+            float *dst = buf + i * fs * fs + j * fs;
+            for (x=0; x<w; x+=stride) {
+                for (k=0; k<fs; k++) dst[k] = (unsigned)(x - pad + k) < (unsigned)w && (unsigned)(y - pad + j) < (unsigned)h ? src[k] : 0;
+                src += stride, dst += ALIGN(c * fs * fs, 4);
             }
-        } while (--i);
-    } else {
-        i = fs * fs * c;
-        j = k = 0;
-        do {
-            *buf++ = ((unsigned)(j - pad + y) < (unsigned)h && (unsigned)(k - pad + x) < (unsigned)w) ? *data : 0; data++;
-            if (++k == fs) {
-                if (1)         { k = 0; data += w - fs; }
-                if (++j == fs) { j = 0; data += w * (h - fs); }
-            }
-        } while (--i);
+        }
     }
 }
 
@@ -382,18 +372,18 @@ static void layer_groupconv_forward(NET *net, LAYER *ilayer, LAYER *olayer)
     fn     = ilayer->fn/ ilayer->groups;
     walign = ALIGN(ilayer->fs * ilayer->fs * ic, 4);
     ftsize = walign + 4;
-    if (net->cnnbufsize < walign) {
-        net->cnnbufsize = walign;
+    if (net->cnnbufsize < walign * olayer->w) {
+        net->cnnbufsize = walign * olayer->w;
         free(net->cnntempbuf); net->cnntempbuf = malloc(net->cnnbufsize * sizeof(float));
         if (net->cnntempbuf == NULL) { printf("failed to allocate memory for cnntempbuf !"); return; }
     }
 
     for (g=0; g<ilayer->groups; g++) {
         for (y=0; y<olayer->h; y++) {
+            im2row(datai, ilayer->w, ilayer->h, ic, ilayer->pad, ilayer->fs, ilayer->stride, y * ilayer->stride, net->cnntempbuf);
             for (x=0; x<olayer->w; x++) {
-                im2row(datai, ilayer->w, ilayer->h, ic, ilayer->pad, ilayer->fs, x * ilayer->stride, y * ilayer->stride, net->cnntempbuf);
                 for (c=0; c<oc; c++) {
-                    for (sum=0,i=0; i<walign; i++) sum += dataf[c * ftsize + i] * net->cnntempbuf[i];
+                    for (sum=0,i=0; i<walign; i++) sum += dataf[c * ftsize + i] * net->cnntempbuf[x * walign + i];
                     if (ilayer->batchnorm) sum = (sum - dataf[c * ftsize + walign + 1]) * dataf[c * ftsize + walign + 2];
                     datao[c * olayer->w * olayer->h + y * olayer->w + x] = activate(sum + dataf[c * ftsize + walign + 0], ilayer->activation);
                 }
