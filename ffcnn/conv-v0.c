61bdf6580dcaff41314de7b78fb996bc2b44aaf3
chenk <chenk@apical.com.cn>
2025-11-28 15:13:13 +0800

    优化卷积代码，性能又有小幅度提升

--- ffcnn/conv-v0.c
+++ ffcnn/conv-v0.c
@@ -4,6 +4,32 @@
 #include "utils.h"
 #include "conv.h"
 
+static void convolution(float *datai, float *dataf, float *datao,
+                        int iw, int ih, int ic, int ipad, int istride,
+                        int fs, int ftsize, int ow, int oh, int oc, int activation,
+                        float *gc_buffer)
+{
+    for (int y = 0; y < oh; y++) {
+        for (int x = 0; x < ow; x++) {
+            for (int c = 0; c < oc; c++) {
+                float sum = 0;
+                for (int i = 0; i < ic; i++) {
+                    for (int j = 0; j < fs; j++) {
+                        for (int k = 0; k < fs; k++) {
+                            int ix = x * istride - ipad + k;
+                            int iy = y * istride - ipad + j;
+                            if ((unsigned)ix < (unsigned)iw && (unsigned)iy < (unsigned)ih) {
+                                sum += datai[i * iw * ih + ix + iy * iw] * dataf[c * (ftsize + 4) + i * fs * fs + k + j * fs];
+                            }
+                        }
+                    }
+                }
+                datao[c * ow * oh + y * ow + x] = activate(sum * dataf[c * (ftsize + 4) + ftsize + 0] + dataf[c * (ftsize + 4) + ftsize + 1], activation);
+            }
+        }
+    }
+}
+
 // datai - input tensor iw * ih * ic divide to ig groups, with ipad & istride
 // dataf - cnn filter, fs * fs size, fn number
 // datao - output tensor ow * oh size
@@ -12,34 +38,15 @@ void groupconv(float *datai, float *dataf, float *datao,
                int fs, int fn, int ow, int oh, int oc, int activation,
                float **gc_buffer, int *gc_bufsize)
 {
-    int gc_ic = ic / ig; // group conv ic
-    int gc_oc = oc / ig; // group conv oc
-    int gc_fn = fn / ig; // group conv fn
-    int walign = ALIGN(fs * fs * gc_ic, 4);
-    int ftsize = walign + 4;
+    int gc_ic  = ic / ig; // group conv ic
+    int gc_oc  = oc / ig; // group conv oc
+    int gc_fn  = fn / ig; // group conv fn
+    int ftsize = ALIGN(fs * fs * gc_ic, 4);
 
     for (int g = 0; g < ig; g++) {
-        for (int y = 0; y < oh; y++) {
-            for (int x = 0; x < ow; x++) {
-                for (int c = 0; c < gc_oc; c++) {
-                    float sum = 0;
-                    for (int i = 0; i < gc_ic; i++) {
-                        for (int j = 0; j < fs; j++) {
-                            for (int k = 0; k < fs; k++) {
-                                int ix = x * istride - ipad + k;
-                                int iy = y * istride - ipad + j;
-                                if ((unsigned)ix < (unsigned)iw && (unsigned)iy < (unsigned)ih) {
-                                    sum += datai[i * iw * ih + ix + iy * iw] * dataf[c * ftsize + i * fs * fs + k + j * fs];
-                                }
-                            }
-                        }
-                    }
-                    datao[c * ow * oh + y * ow + x] = activate(sum * dataf[c * ftsize + walign + 0] + dataf[c * ftsize + walign + 1], activation);
-                }
-            }
-        }
+        convolution(datai, dataf, datao, iw, ih, gc_ic, ipad, istride, fs, ftsize, ow, oh, gc_oc, activation, *gc_buffer);
         datai += iw * ih * gc_ic;
         datao += ow * oh * gc_oc;
-        dataf += ftsize * gc_fn;
+        dataf += (ftsize + 4) * gc_fn;
     }
 }
--- ffcnn/conv-v1.c
+++ ffcnn/conv-v1.c
@@ -4,7 +4,7 @@
 #include "utils.h"
 #include "conv.h"
 
-static void im2row(float *img, int w, int h, int c, int pad, int fs, int stride, int walign, int y, float *buf)
+static void im2row(float *img, int w, int h, int c, int pad, int fs, int stride, int ftsize, int y, float *buf)
 {
     float *src = img + (y - pad) * w - pad, *dst = buf;
     int    i, j, k, x;
@@ -13,7 +13,7 @@ static void im2row(float *img, int w, int h, int c, int pad, int fs, int stride,
             float *tmp = dst;
             for (x = 0; x < w; x += stride) {
                 for (k = 0; k < fs; k++) dst[k] = (unsigned)(x - pad + k) < (unsigned)w && (unsigned)(y - pad + j) < (unsigned)h ? src[k] : 0;
-                src += stride, dst += walign;
+                src += stride, dst += ftsize;
             }
             src += w - x, dst = tmp + fs;
         }
@@ -21,36 +21,46 @@ static void im2row(float *img, int w, int h, int c, int pad, int fs, int stride,
     }
 }
 
+static void convolution(float *datai, float *dataf, float *datao,
+                        int iw, int ih, int ic, int ipad, int istride,
+                        int fs, int ftsize, int ow, int oh, int oc, int activation,
+                        float *gc_buffer)
+{
+    for (int y = 0; y < oh; y++) {
+        im2row(datai, iw, ih, ic, ipad, fs, istride, ftsize, y * istride, gc_buffer);
+        for (int x = 0; x < ow; x++) {
+            for (int c = 0; c < oc; c++) {
+                float sum = 0;
+                for (int i = 0; i < ftsize; i++) sum += dataf[c * (ftsize + 4) + i] * gc_buffer[x * ftsize + i];
+                datao[c * ow * oh + y * ow + x] = activate(sum * dataf[c * (ftsize + 4) + ftsize + 0] + dataf[c * (ftsize + 4) + ftsize + 1], activation);
+            }
+        }
+    }
+}
+
+// datai - input tensor iw * ih * ic divide to ig groups, with ipad & istride
+// dataf - cnn filter, fs * fs size, fn number
+// datao - output tensor ow * oh size
 void groupconv(float *datai, float *dataf, float *datao,
                int iw, int ih, int ic, int ig, int ipad, int istride,
                int fs, int fn, int ow, int oh, int oc, int activation,
                float **gc_buffer, int *gc_bufsize)
 {
-    int gc_ic = ic / ig; // group conv ic
-    int gc_oc = oc / ig; // group conv oc
-    int gc_fn = fn / ig; // group conv fn
-    int walign = ALIGN(fs * fs * gc_ic, 4);
-    int ftsize = walign + 4;
+    int gc_ic  = ic / ig; // group conv ic
+    int gc_oc  = oc / ig; // group conv oc
+    int gc_fn  = fn / ig; // group conv fn
+    int ftsize = ALIGN(fs * fs * gc_ic, 4);
 
-    if (*gc_bufsize < walign * ow) {
-        *gc_bufsize = walign * ow;
+    if (*gc_bufsize < ftsize * ow) {
+        *gc_bufsize = ftsize * ow;
         free(*gc_buffer); *gc_buffer = malloc(*gc_bufsize * sizeof(float));
         if (*gc_buffer == NULL) { printf("failed to allocate memory for cnntempbuf !"); return; }
     }
 
     for (int g = 0; g < ig; g++) {
-        for (int y = 0; y < oh; y++) {
-            im2row(datai, iw, ih, gc_ic, ipad, fs, istride, walign, y * istride, *gc_buffer);
-            for (int x = 0; x < ow; x++) {
-                for (int c = 0; c < gc_oc; c++) {
-                    float sum = 0;
-                    for (int i = 0; i < walign; i++) sum += dataf[c * ftsize + i] * (*gc_buffer)[x * walign + i];
-                    datao[c * ow * oh + y * ow + x] = activate(sum * dataf[c * ftsize + walign + 0] + dataf[c * ftsize + walign + 1], activation);
-                }
-            }
-        }
+        convolution(datai, dataf, datao, iw, ih, gc_ic, ipad, istride, fs, ftsize, ow, oh, gc_oc, activation, *gc_buffer);
         datai += iw * ih * gc_ic;
         datao += ow * oh * gc_oc;
-        dataf += ftsize * gc_fn;
+        dataf += (ftsize + 4) * gc_fn;
     }
 }
